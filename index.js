6969e79f63a86bc43575afb37849b9aee6c7aec0
Kim Shing Chong <chong.kim.shing@gmail.com>
2021-08-21 19:06:38 -0400

    Added in base pre-requisites for command handlind

--- index.js
+++ index.js
@@ -11,5 +11,17 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'beep') {
+		await interaction.reply('Boop!');
+	}
+});
 
 client.login(process.env.CLIENT_TOKEN); //login bot using token
+
