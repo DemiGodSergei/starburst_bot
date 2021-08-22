5cdfb84b2cbe48ff30d5b39d28ac7477bbe7a963
Kim Shing Chong <chong.kim.shing@gmail.com>
2021-08-22 06:55:21 -0400

    Registered /beep to bot

--- command_deploy.js
+++ command_deploy.js
@@ -5,9 +5,10 @@ require('dotenv').config()
 // const { clientId, guildId, token } = require('./config.json');
 
 const commands = [
-	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!!!'),
 	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
 	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+	new SlashCommandBuilder().setName('beep').setDescription('Replies with user boop!')
 ]
 	.map(command => command.toJSON());
 
